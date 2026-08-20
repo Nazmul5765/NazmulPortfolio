51b377a17b8ee4a23ff8795bed1c77a169c1d68a
Nazmul5765 <nazmulhussain@hotmail.co.uk>
2026-08-20 16:27:50 +0100

    chore: add health check endpoint for deployment

--- NazmulPortfolio/Program.cs
+++ NazmulPortfolio/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,5 +23,7 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapHealthChecks("/health");
 
 app.Run();
