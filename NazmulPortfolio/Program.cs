581371672db55257c934bf04b747424482f8bad0
Nazmul5765 <nazmulhussain@hotmail.co.uk>
2026-08-20 15:40:13 +0100

    refactor: remove Render demo warm-up workaround

--- NazmulPortfolio/Program.cs
+++ NazmulPortfolio/Program.cs
@@ -1,14 +1,10 @@
 using NazmulPortfolio.Components;
-using NazmulPortfolio.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
-builder.Services.AddHttpClient();
-builder.Services.AddHostedService<DemoWarmupService>();
 
 var app = builder.Build();
 
@@ -19,8 +15,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
